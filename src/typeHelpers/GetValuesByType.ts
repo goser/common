2bbd3afe96fee6b8a9ab11985ff1509d6b016678
mplacke <agos@gmx.net>
2021-11-27 22:50:42 +0100

    Add GetValuesByType type helper

---
+++ src/typeHelpers/GetValuesByType.ts
@@ -0,0 +1,20 @@
+/**
+ * Get all values of a given mapped type which match the given type.
+ *
+ * Example:
+ * type MyMappedType = {
+ *     a: number
+ *     b: boolean
+ *     c: string
+ *     d: bigint
+ *     e: string
+ *     f: number
+ * }
+ * type MyStrings = GetValuesByType<MyMappedType, string>
+ * // => "c" | "e"
+ *
+ */
+export type GetValuesByType<T extends object, V> = {
+    [K in keyof T]: T[K] extends V ? K : never
+}[keyof T];
+
