43554c9ed68f6976ef0e14edf8ed743a3102c48e
mplacke <agos@gmx.net>
2025-03-27 09:51:21 +0100

    Add FlattenProps type

---
+++ src/typeHelpers/FlattenProps.ts
@@ -0,0 +1,31 @@
+/**
+ * When union types get intersected with common properties, 
+ * the type can get a bit messy. This helper type "merges" the common properties 
+ * with the members of the union
+ * 
+ * Example:
+ *     type IntersectedUnion = {common: string} & ({special1: string} | {special2: string})
+ *
+ *     // this shows as:
+ *     // type IntersectedUnion = {
+ *     //     common: string;
+ *     // } & ({
+ *     //     special1: string;
+ *     // } | {
+ *     //     special2: string;
+ *     // })
+ * 
+ *     type SimpleUnion = FlattenProps<IntersectedUnion>
+ * 
+ *     // this shows as: 
+ *     // type SimpleUnion = {
+ *     //     common: string;
+ *     //     special1: string;
+ *     // } | {
+ *     //     common: string;
+ *     //     special2: string;
+ *     // }
+ */
+export type FlattenProps<T> = {
+    [P in keyof T]: T[P]
+} & {} // the & {} is needed here to force the flattening
